9c732756116baee6a2e166503eadd5e276de727f
Davi Aron <daviaronalfa@gmail.com>
2024-10-16 22:11:20 -0300

    definindo como meus dados vao entrar no banco através do seed

--- database/factories/ProjectFactory.php
+++ database/factories/ProjectFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\User;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\project>
@@ -13,12 +14,12 @@ class ProjectFactory extends Factory
     public function definition(): array
     {
         return [
-            'title' => fake()->words(5),
+            'title' => collect(fake()->words(5))->join(' '),
             'description' => fake()->randomHtml(),
             'ends_at' => fake()->dateTimeBetween('now', '+ 3 days'),
-            'status' ,
-            'tech_stack',
-            'created_by',
+            'status' => fake()->randomElement(['open', 'closed']),
+            'tech_stack' => fake()->randomElements(['React', 'PHP', 'Laravel', 'Vue', 'Tailwind', 'JavaScript', 'NextJS', 'Python'], random_int(1, 5)),
+            'created_by' => User::factory(),
         ];
     }
 }
