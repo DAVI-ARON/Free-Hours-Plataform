9f9c03ccba9ce6874a80e131c1087dc50112c62e
Davi Aron <daviaronalfa@gmail.com>
2024-10-16 22:58:46 -0300

    alterando forma de criacao de seeds - criando projetos a partir de usuarios existentes e criando propostas em cima dos projetos existentes

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\User;
 use App\Models\Project;
+use App\Models\Proposal;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -15,6 +16,13 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         User::factory(200)->create();
-        Project::factory(10)->create();
+
+        User::query()->inRandomOrder()->limit(10)->get()
+            ->each(function (User $user) {
+                $project = Project::factory()->create([
+                    'created_by' => $user->id,
+                ]);
+                Proposal::factory()->count(random_int(4, 45))->create(['project_id' => $project->id]);
+            });
     }
 }
